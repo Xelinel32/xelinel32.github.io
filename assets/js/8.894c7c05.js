f52554a02b859a87d01991c49ee231daac048b1c
xelinel32 <artemsedlyar99@gmail.com>
2020-09-25 11:00:04 +0300

    del

--- assets/js/8.894c7c05.js
+++ assets/js/8.894c7c05.js
@@ -1,1 +1,144 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{361:function(t,e,a){"use strict";a.r(e);var s=a(25),i=Object(s.a)({},(function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("ContentSlotsDistributor",{attrs:{"slot-key":t.$parent.slotKey}},[a("h1",{attrs:{id:"resume"}},[a("a",{staticClass:"header-anchor",attrs:{href:"#resume"}},[t._v("#")]),t._v(" Resume")]),t._v(" "),a("p",[t._v("I start as self-educated HTML-coder and worked in web-studio, one year, where I created landing pages or multi-page hard templates using BEM naming, jQuery and native JS ES5/ES6.")]),t._v(" "),a("p",[t._v("I have confident knowledge of all types of layout and SCSS capabilities. I set up Gulp / Webpack configurations for all projects. Familiar with many libraries and plugins that speed up development.")]),t._v(" "),a("h2",{attrs:{id:"frontend-skills"}},[a("a",{staticClass:"header-anchor",attrs:{href:"#frontend-skills"}},[t._v("#")]),t._v(" Frontend skills")]),t._v(" "),a("ul",[a("li",[t._v("JavaScript (ES6+)")]),t._v(" "),a("li",[t._v("Vue.js 2.x, RestAPI Concept")]),t._v(" "),a("li",[t._v("Vuex / Router")]),t._v(" "),a("li",[t._v("Stylus / SCSS / SASS / styled-components")]),t._v(" "),a("li",[t._v("PugJS (Jade)")]),t._v(" "),a("li",[t._v("Vuetify, Twitter Bootstrap, Material UI")]),t._v(" "),a("li",[t._v("Webpack / Gulp / Parcel")])]),t._v(" "),a("h2",{attrs:{id:"another-skills"}},[a("a",{staticClass:"header-anchor",attrs:{href:"#another-skills"}},[t._v("#")]),t._v(" Another skills")]),t._v(" "),a("ul",[a("li",[t._v("Git / GitHub / Bitbucket / GitLab")]),t._v(" "),a("li",[t._v("GitFlow concept")]),t._v(" "),a("li",[t._v("BEM (CSS)")]),t._v(" "),a("li",[t._v("MySQL / MongoDB")]),t._v(" "),a("li",[t._v("JSON")])]),t._v(" "),a("h3",{attrs:{id:"💢"}},[a("a",{staticClass:"header-anchor",attrs:{href:"#💢"}},[t._v("#")]),t._v(" 💢")]),t._v(" "),a("div",{staticClass:"custom-block tip"},[a("p",{staticClass:"custom-block-title"},[t._v("Now")]),t._v(" "),a("p",[t._v("Today I work in a freelance place, implementing and modifying existing sites and SPA using Vue.js/Nuxt.js/Vuetify.")])]),t._v(" "),a("p",[t._v("I also practice every day and do my own projects. "),a("strong",[a("a",{attrs:{href:"github.com/xelinel32"}},[t._v("github")])])]),t._v(" "),a("p",[t._v("My account at freelancehunt. "),a("strong",[a("a",{attrs:{href:""}},[t._v("hunt")])])]),t._v(" "),a("blockquote",[a("p",[t._v("Ready to complete technical task! 🖤")])])])}),[],!1,null,null,null);e.default=i.exports}}]);
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [8],
+  {
+    361: function (t, e, a) {
+      'use strict'
+      a.r(e)
+      var s = a(25),
+        i = Object(s.a)(
+          {},
+          function () {
+            var t = this,
+              e = t.$createElement,
+              a = t._self._c || e
+            return a(
+              'ContentSlotsDistributor',
+              { attrs: { 'slot-key': t.$parent.slotKey } },
+              [
+                a('h1', { attrs: { id: 'resume' } }, [
+                  a(
+                    'a',
+                    {
+                      staticClass: 'header-anchor',
+                      attrs: { href: '#resume' },
+                    },
+                    [t._v('#')]
+                  ),
+                  t._v(' Resume'),
+                ]),
+                t._v(' '),
+                a('p', [
+                  t._v(
+                    'I start as self-educated HTML-coder and worked in web-studio, one year, where I created landing pages or multi-page hard templates using BEM naming, jQuery and native JS ES5/ES6.'
+                  ),
+                ]),
+                t._v(' '),
+                a('p', [
+                  t._v(
+                    'I have confident knowledge of all types of layout and SCSS capabilities. I set up Gulp / Webpack configurations for all projects. Familiar with many libraries and plugins that speed up development.'
+                  ),
+                ]),
+                t._v(' '),
+                a('h2', { attrs: { id: 'frontend-skills' } }, [
+                  a(
+                    'a',
+                    {
+                      staticClass: 'header-anchor',
+                      attrs: { href: '#frontend-skills' },
+                    },
+                    [t._v('#')]
+                  ),
+                  t._v(' Frontend skills'),
+                ]),
+                t._v(' '),
+                a('ul', [
+                  a('li', [t._v('JavaScript (ES6+)')]),
+                  t._v(' '),
+                  a('li', [t._v('Vue.js 2.x, RestAPI Concept')]),
+                  t._v(' '),
+                  a('li', [t._v('Vuex / Router')]),
+                  t._v(' '),
+                  a('li', [t._v('Stylus / SCSS / SASS')]),
+                  t._v(' '),
+                  a('li', [t._v('PugJS (Jade)')]),
+                  t._v(' '),
+                  a('li', [t._v('Vuetify, Twitter Bootstrap, Material UI')]),
+                  t._v(' '),
+                  a('li', [t._v('Webpack / Gulp / Parcel')]),
+                ]),
+                t._v(' '),
+                a('h2', { attrs: { id: 'another-skills' } }, [
+                  a(
+                    'a',
+                    {
+                      staticClass: 'header-anchor',
+                      attrs: { href: '#another-skills' },
+                    },
+                    [t._v('#')]
+                  ),
+                  t._v(' Another skills'),
+                ]),
+                t._v(' '),
+                a('ul', [
+                  a('li', [t._v('Git / GitHub / Bitbucket / GitLab')]),
+                  t._v(' '),
+                  a('li', [t._v('GitFlow concept')]),
+                  t._v(' '),
+                  a('li', [t._v('BEM (CSS)')]),
+                  t._v(' '),
+                  a('li', [t._v('MySQL / MongoDB')]),
+                  t._v(' '),
+                  a('li', [t._v('JSON')]),
+                ]),
+                t._v(' '),
+                a('h3', { attrs: { id: '💢' } }, [
+                  a(
+                    'a',
+                    { staticClass: 'header-anchor', attrs: { href: '#💢' } },
+                    [t._v('#')]
+                  ),
+                  t._v(' 💢'),
+                ]),
+                t._v(' '),
+                a('div', { staticClass: 'custom-block tip' }, [
+                  a('p', { staticClass: 'custom-block-title' }, [t._v('Now')]),
+                  t._v(' '),
+                  a('p', [
+                    t._v(
+                      'Today I work in a freelance place, implementing and modifying existing sites and SPA using Vue.js/Nuxt.js/Vuetify.'
+                    ),
+                  ]),
+                ]),
+                t._v(' '),
+                a('p', [
+                  t._v('I also practice every day and do my own projects. '),
+                  a('strong', [
+                    a('a', { attrs: { href: 'github.com/xelinel32' } }, [
+                      t._v('github'),
+                    ]),
+                  ]),
+                ]),
+                t._v(' '),
+                a('p', [
+                  t._v('My account at freelancehunt. '),
+                  a('strong', [
+                    a('a', { attrs: { href: '' } }, [t._v('hunt')]),
+                  ]),
+                ]),
+                t._v(' '),
+                a('blockquote', [
+                  a('p', [t._v('Ready to complete technical task! 🖤')]),
+                ]),
+              ]
+            )
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        )
+      e.default = i.exports
+    },
+  },
+])
